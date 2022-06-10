228c58e83c8ab92b6334c24e46a76e86a2e3738b
samj1046 <59887744+samj1046@users.noreply.github.com>
2022-06-10 17:40:31 +0100

    Add files via upload

---
+++ Controllers/ShopController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace coursework2.Controllers
+{
+    public class ShopController : Controller
+    {
+        // GET: Shop
+       
+        
+            public ActionResult Shop()
+            {
+                return View();
+            }
+        public ActionResult Sell()
+        {
+            return View();
+        }
+        public ActionResult Checkout()
+        {
+            return View();
+        }
+    }
+}
